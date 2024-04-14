a62ac8ab7577de9d959977698e2c2ee1a09cf39b
MartinP1199 <potociarmartin@gmail.com>
2024-04-14 13:37:27 +0200

    sempr: general menu option

--- SemPr/ConsoleApp.h
+++ SemPr/ConsoleApp.h
@@ -8,6 +8,7 @@
 class ConsoleApp {
 private:
 	CliMenu main_menu;
+
 public:
 	ConsoleApp() : main_menu("Main menu") {};
 	void Start();
@@ -15,33 +16,45 @@ public:
 
 void ConsoleApp::Start() {
 
-	// ========== main menu ==========
+	// ========== support items ==========
 	string dataPath = "C:\\Users\\potoc\\source\\repos\\BeardedCrackie\\DataStructures\\SemPr\\RT.csv";
 	ImplicitSequence<NetworkRoute*>* networkRoutes = new ImplicitSequence<NetworkRoute*>();
+
+	MenuActionItem clearSequence = MenuActionItem("Clear Sequence", [&]()
+		{
+			for (size_t i = 0; i < networkRoutes->size(); i++)
+			{
+				delete networkRoutes->access(i)->data_;
+			}
+			networkRoutes->clear();
+			delete networkRoutes;
+			networkRoutes = new ImplicitSequence<NetworkRoute*>();
+		});
+
+	// ========== main menu ==========
+
 	AlgorithmProcessor algp = AlgorithmProcessor();
 
 	MenuActionItem itemPrintSequence = MenuActionItem("Print Sequence", [&]()
 		{
-			algp.processRouteTable(networkRoutes->begin(), networkRoutes->end(), [&](NetworkRoute* rt) {
+			ImplicitSequence<NetworkRoute*>* tmp = algp.processRouteTable(networkRoutes->begin(), networkRoutes->end(), [&](NetworkRoute* rt) {
 				rt->printRoute();
 				return true;
 				});
+			delete networkRoutes;
+			networkRoutes = tmp;
 		});
 	main_menu.AddItem(&itemPrintSequence);
 
 	this->main_menu.AddItem(new MenuActionItem("Load CSV", [&]()
 		{
-			networkRoutes->clear();
-			Loader routeLoader = Loader();
-			routeLoader.load("C:\\Users\\potoc\\source\\repos\\BeardedCrackie\\DataStructures\\SemPr\\RT.csv", *networkRoutes);
+			this->main_menu.RunItem(&clearSequence);
+			networkRoutes = new ImplicitSequence<NetworkRoute*>();
+			Loader().load("C:\\Users\\potoc\\source\\repos\\BeardedCrackie\\DataStructures\\SemPr\\RT.csv", *networkRoutes);
 		}
 	));
 
-	this->main_menu.AddItem(new MenuActionItem("Clear Sequence", [&]()
-		{
-			networkRoutes->clear();
-		}
-	));
+	this->main_menu.AddItem(&clearSequence);
 
 
 	// ========== level 1 ==========
@@ -55,7 +68,7 @@ void ConsoleApp::Start() {
 			string ipAddr;
 			std::cin >> ipAddr;
 			std::bitset<32> compareRt = NetworkRoute::ipToBitset(ipAddr);
-			networkRoutes = algp.processRouteTable(networkRoutes->begin(), networkRoutes->end(), [&](NetworkRoute* rt) {
+			ImplicitSequence<NetworkRoute*>* tmp = algp.processRouteTable(networkRoutes->begin(), networkRoutes->end(), [&](NetworkRoute* rt) {
 				std::bitset<32> parent = rt->getNetworkAddress();
 				for (int i = 0; i < rt->getPrefix(); i++)
 				{
@@ -65,6 +78,8 @@ void ConsoleApp::Start() {
 				}
 				return true;
 				});
+			delete networkRoutes;
+			networkRoutes = tmp;
 		}));
 
 	level1.AddItem(new MenuActionItem("matchLifetime", [&]()
@@ -78,12 +93,15 @@ void ConsoleApp::Start() {
 			int lowerBorder = stoi(lower);
 			int higherBorder = stoi(upper);
 
-			networkRoutes = algp.processRouteTable(networkRoutes->begin(), networkRoutes->end(), [&](NetworkRoute* rt) {
+			ImplicitSequence<NetworkRoute*>* tmp = algp.processRouteTable(networkRoutes->begin(), networkRoutes->end(), [&](NetworkRoute* rt) {
 				if (rt->getTtl() >= lowerBorder && rt->getTtl() <= higherBorder) {
+					rt->printRoute();
 					return true;
 				}
 				return false;
 				});
+			delete networkRoutes;
+			networkRoutes = tmp;
 		}));
 
 
