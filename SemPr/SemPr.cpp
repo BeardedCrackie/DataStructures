5279db352d29e4bc692ca0e3e2c7a2bb5aa3f3f7
MartinP1199 <potociarmartin@gmail.com>
2024-04-14 15:32:25 +0200

    sempr: chores

--- SemPr/SemPr.cpp
+++ SemPr/SemPr.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include "ConsoleApp.h"
 
+
 using namespace ds::amt;
 
 int main()
@@ -12,62 +13,6 @@ int main()
     initHeapMonitor();
     SetConsoleOutputCP(1250);
     SetConsoleCP(1250);
-
-    //getline(loadedStream, value, ';'); //source, not important so skip
-    //getline(loadedStream, value, '/'); //address
-    //newRoute->setNetworkAddress(value);
-    //getline(loadedStream, value, ';'); //prefix
-    //newRoute->setNetworkPrefix(stoi(value));
-    //getline(loadedStream, value, ';'); //metrics, not important so skip
-    //getline(loadedStream, value, ';'); //next-hop
-    //newRoute->setNextHop(value.substr(3));
-    //getline(loadedStream, value, ';'); //time in string
-    //newRoute->setTtl(value);
-    //delete newRoute;
-    //newRoute = 0;
-    //std::cout << "--load routes--" << std::endl;
-    //ImplicitSequence<NetworkRoute*> routeSequence = ImplicitSequence<NetworkRoute*>();
-    //
-    //Loader routeLoader = Loader();
-    //routeLoader.load("C:\\Users\\potoc\\source\\repos\\BeardedCrackie\\DataStructures\\SemPr\\RT.csv", routeSequence);
-    //
-    //std::cout << "--routes algorithm--" << std::endl;
-    //
-    //AlgorithmProcessor algp = AlgorithmProcessor();
-    ////algp.processData(routeSequence.begin(), routeSequence.end(), [&](NetworkRoute* rt) {
-    ////        rt->printRoute();
-    ////        return true;
-    ////    });
-    //
-    //std::bitset<32> compareRt = NetworkRoute::ipToBitset("1.0.16.0");
-    //
-    //std::cout << "--matchWithAddress--" << std::endl;
-    //algp.processRouteTable(routeSequence.begin(), routeSequence.end(), [&](NetworkRoute* rt) {
-    //    //matchWithAddress
-    //    std::bitset<32> parent = rt->getNetworkAddress();
-    //    for (int i = 0; i < rt->getPrefix(); i++)
-    //    {
-    //        if (parent[31 - i] != compareRt[31 - i]) {
-    //            return false;
-    //        }
-    //    }
-    //    rt->printRoute();
-    //    return true;
-    //});
-    //
-    //int lowerBorder = 100;
-    //int higherBorder = 10000;
-    //std::cout << "--matchLifetime--" << std::endl;
-    //algp.processRouteTable(routeSequence.begin(), routeSequence.end(), [&](NetworkRoute* rt) {
-    //    //matchLifetime
-    //    if (rt->getTtl() >= 100 && rt->getTtl() <= higherBorder) {
-    //        rt->printRoute();
-    //        return true;
-    //    }
-    //    return false;
-    //});
-    //
-    //std::cout << "Done";
 
     ConsoleApp().Start();
 
